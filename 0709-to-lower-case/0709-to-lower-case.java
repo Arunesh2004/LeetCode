be436e02b4c90b455a0750c30f82d2318d57ecf4
Arunesh Sharma <sharmaarunesh07@gmail.com>
2024-09-26 17:45:43 +0530

    Time: 0 ms (100%), Space: 42 MB (10.64%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,7 +1,24 @@
 class Solution {
     public String toLowerCase(String s) {
 
-        return s.toLowerCase();
+        StringBuilder res = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+
+            if(ch >= 'A' && ch <= 'Z'){
+
+                res.append((char)(ch+32));
+
+            }
+
+            else{
+
+                res.append(ch);
+
+            }
+        }
+
+        return res.toString();
 
         
     }
