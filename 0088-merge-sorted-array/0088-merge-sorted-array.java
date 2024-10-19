e93fa6fbd14928ec96b658bc40766a1d23f8c85e
Arunesh Sharma <sharmaarunesh07@gmail.com>
2024-10-19 19:31:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(i >= 0 && j >= 0){
+
+            if(arr1[i] > arr2[j]  && arr1[i] != 0){
+
+                arr1[k] = arr1[i];
+                i--;
+
+            }
+            else if(arr2[j] != 0){ 
+
+                arr1[k] = arr2[j];
+                j--;
+
+            }
+
+            k--;
+        }
+
+        while(j >= 0){
+
+            arr1[k] = arr2[j];
+            k--;
+            j--;
+
+        }
+
+    }
+}
