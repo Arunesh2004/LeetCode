e4313cd56c755d169a01dfdaf638a792155efb87
Arunesh Sharma <sharmaarunesh07@gmail.com>
2024-12-04 22:52:39 +0530

    Time: 0 ms (100%), Space: 41.1 MB (80.06%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        
+        if(s.length() != goal.length()){
+
+            return false;
+
+        }
+
+        String s2 = s + s;
+
+        return s2.contains(goal);
+    }
+}
