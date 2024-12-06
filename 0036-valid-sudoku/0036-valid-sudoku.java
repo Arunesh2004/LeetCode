52e4e2ae13ecd0e7257e291253bfc6cbc979771a
Arunesh Sharma <sharmaarunesh07@gmail.com>
2024-12-06 21:34:55 +0530

    Time: 1 ms (100%), Space: 44.8 MB (29.63%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+
+        for(int i = 0 ; i < 9 ; i++){
+
+            for(int j = 0 ; j < 9 ; j++){
+
+                if(board[i][j] == '.'){
+
+                    continue;
+
+                }
+
+                int num = board[i][j] - '1';
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (row[i][num] || col[j][num] || box[boxIndex][num]) {
+
+                    return false;
+
+                }
+
+                row[i][num] = true;
+                col[j][num] = true;
+                box[boxIndex][num] = true;
+
+            }
+        }
+
+        return true;
+
+    }
+}
