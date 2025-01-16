23ec68fb7a4174e16a00023c4f0550cfaa0b0686
Arunesh Sharma <sharmaarunesh07@gmail.com>
2025-01-16 18:11:33 +0530

    Time: 1 ms (100%), Space: 61.3 MB (68.97%) - LeetHub

---
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+
+        int xor1 = 0, xor2 = 0;
+
+        for (int num : nums1){
+
+            xor1 ^= num;
+
+        }
+
+        for (int num : nums2){
+
+            xor2 ^= num;
+
+        }
+
+        int result = 0;
+
+        if (nums1.length % 2 != 0) {
+
+            result ^= xor2;
+
+        }
+        if (nums2.length % 2 != 0) {
+
+            result ^= xor1;
+
+        }
+
+        return result;
+        
+    }
+}
