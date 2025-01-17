3c030dfafee65144fc281f65688d858ade48235e
Arunesh Sharma <sharmaarunesh07@gmail.com>
2025-01-17 19:28:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,32 @@
+class Solution{
+    public int countPrimes(int n){
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++){
+
+            if (isPrime(i)){
+
+                count++; 
+
+            }
+        }
+
+        return count;
+
+    }
+
+    private boolean isPrime(int num){
+
+        if (num < 2) return false; 
+
+        for (int i = 2; i * i <= num; i++){ 
+
+            if (num % i == 0) return false; 
+
+        }
+
+        return true;
+
+    }
+}
