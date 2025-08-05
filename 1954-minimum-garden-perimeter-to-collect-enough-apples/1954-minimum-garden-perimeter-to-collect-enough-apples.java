5de17d556e23f446cad2a7337d49caa8f8d5072d
Arunesh Sharma <sharmaarunesh07@gmail.com>
2025-08-05 12:20:18 +0530

    Time: 10 ms (47.54%), Space: 40.5 MB (83.06%) - LeetHub

---
+++ 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long minimumPerimeter(long neededApples) {
+        
+        long count = 0;
+        long sumA = 0;
+
+        while(sumA < neededApples){
+
+            count++;
+            sumA = sumA + (12*count*count);
+            // sumA = 2 * count * ( count + 1 ) * ( 2 * count + 1 );
+            
+        }
+
+        return 8*count;
+    }
+}
