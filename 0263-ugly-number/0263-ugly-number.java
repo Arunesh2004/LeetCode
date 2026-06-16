548f0b07cabcf7b6c6b9827dbd2888d931b3ab66
Arunesh Sharma <sharmaarunesh07@gmail.com>
2026-06-16 21:47:30 +0530

    Time: 1 ms (99.14%), Space: 42.9 MB (14.73%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isUgly(int n) {
+
+        if(n <= 0) {
+            return false;
+        }
+
+        while(n % 2 == 0) {
+            n /= 2;
+        }
+
+        while(n % 3 == 0) {
+            n /= 3;
+        }
+
+        while(n % 5 == 0) {
+            n /= 5;
+        }
+
+        return n == 1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
